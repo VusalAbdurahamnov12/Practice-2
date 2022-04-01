8021fd2088a2cdaf52e7930de91f4d39470b27fb
test <sh13ld001@gmail.com>
2022-04-01 09:45:28 +0400

    first

---
+++ tttt/Models/Group.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tttt.Exceptions;
+
+namespace tttt.Models
+{
+    class Group
+    {
+        private int _studentLimit;
+        private string _GroupNo;
+        private bool _upper1 =false;
+        private bool _digit1 =false;
+        private bool _digit2 =false;
+        private bool _digit3 =false;
+
+        public int StudentLimit { get { return _studentLimit; }
+            set
+            {
+                if (value > 5 || value < 18) _studentLimit = value;
+                else throw new NotAvaiavleException("Limiti kecdiniz!");
+            } 
+        }
+        public string GroupNo {
+            get { return _GroupNo; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) || !string.IsNullOrWhiteSpace(value) && value.Length >=5) _GroupNo = value;
+                else throw new NotAvaiavleException("Limiti kecdiniz!");
+            }
+        }
+        private Student[] student = new Student[0];
+        public bool CheckGroupNo(string groupNo)
+        {
+            if (!String.IsNullOrEmpty(groupNo) && !String.IsNullOrWhiteSpace(groupNo) && groupNo.Length >= 5)
+            {
+                int j = 1; //1 ci index
+                char[] charArr = groupNo.ToCharArray();
+                for (int i = 0; i < charArr.Length; i++)
+                {
+                    if (Char.IsUpper(charArr[i]) && Char.IsUpper(charArr[j])) return _upper1 = true;
+                    else if (Char.IsDigit(charArr[i])) return _digit1 = true;
+                    else if (Char.IsDigit(charArr[i])) return _digit2 = true;
+                    else if (Char.IsDigit(charArr[i])) return _digit3 = true;
+                    if (_upper1 == true && _digit1 == true && _digit2 == true && _digit3 == true) return true; break;
+                }
+            }return false; throw new NotAvaiavleException("Grup nomresi sehvdir");
+
+        }
+        public void AddStudent(Student stu)
+        {
+            if (StudentLimit > student.Length)
+            {
+                Array.Resize(ref student, student.Length + 1);
+                student[student.Length + 1] = stu;
+            }
+            else throw new NotAvaiavleException("Limit kecildi");
+        }
+        public Group(string groupNo,int studentLimit)
+        {
+            GroupNo=groupNo;
+            StudentLimit=studentLimit;
+        }
+        public void GetAllStudents() 
+        {
+            for (int i = 0; i < student.Length; i++)
+            {
+                Console.WriteLine($"ID-{student[i].Id}\nFullname-{student[i].Fullname}\nPoint-{student[i].Point}");
+            }
+        }
+    }
+}
---
+++ tttt/Models/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tttt.Exceptions;
+
+namespace tttt.Models
+{
+    public class Student
+    {
+        private static int _id;
+        public int Id { get; }
+        private string _Fullname;
+        private int _Point;
+        public string Fullname
+        {
+            get { return _Fullname; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value)) _Fullname = value;
+                else throw new NotAvaiavleException("Bosluq daxil etmeyin!!");
+            }
+        }
+        public int Point
+        {
+            get { return _Point; }
+            set
+            {
+                if (value >= 100) _Point = value;
+                else throw new NotAvaiavleException("100");
+            }
+        }
+        static Student()
+        {
+            _id = 0;
+        }
+        public Student()
+        {
+            _id++;
+            Id = _id;
+        }
+        public Student(string fullname,int point) :this()
+        {
+            Fullname = fullname;
+            Point = point;
+        }
+
+    }
+}
